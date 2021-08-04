08e3af73bdab5f7cb493ddbb4ce4a7c3885b6908
leonardo <leonardo.ferreira01@fatec.sp.gov.br>
2021-08-04 19:20:39 -0300

    Projeto de Login e autenticação Completo Firebase + Floating Action Menu

--- app/src/main/java/com/playxcodes/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/playxcodes/organizze/activity/PrincipalActivity.java
@@ -3,6 +3,7 @@ package com.playxcodes.organizze.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.playxcodes.organizze.R;
 
@@ -12,5 +13,13 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+    }
+
+    public void adicionarReceita(View view){
+        
+    }
+
+    public void adicionarDespesa(View view){
+
     }
 }
